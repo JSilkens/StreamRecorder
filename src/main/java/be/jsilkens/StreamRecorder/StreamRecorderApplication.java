794a2bdb86e3cc7c4b1ab8345e41c67e51db37d9
Silkens <Johan.Silkens@ordina.be>
2018-03-15 11:34:56 +0100

    init commit

---
+++ src/main/java/be/jsilkens/StreamRecorder/StreamRecorderApplication.java
@@ -0,0 +1,12 @@
+package be.jsilkens.StreamRecorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamRecorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamRecorderApplication.class, args);
+	}
+}
